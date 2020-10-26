6f2a28c15e9683d575e5e321c8cd395cc1c28d72
clement desroches <clement.desroches@mines-paristech.fr>
2020-10-26 11:15:42 +0100

    yes

--- traitement.py
+++ traitement.py
@@ -19,7 +19,7 @@ companies_bdd = list(bdd['BvD_ID_NUMBER'])
 
 
 ### traitement nace
-nace = nace.loc[nace['nace_main_section'] != np.nan, :]
+nace = nace.loc[nace['nace_main_section'].notna(), :]
 companies_nace = list(nace['BVDID'])
 
 ### reindexation et tri en fonction du BVDID
@@ -38,23 +38,27 @@ colonnes_df1 = []
 ## colonnes à ajouter : Nace code, profitability ?, revenue per employees
 
 ### nace 
-df1 = df1.join(nace, how = 'outer')
+df1 = df1.join(nace, how = 'left')
 
 ###revenue per employees
 df1['revenue_per_employees'] = df1['Turnover']/df1['Employees']
 
-
-
 ### merging bdd et df1
-donnees_financieres = []
 
-for i in companies_bdd :
-	if i in companies_nace : 
-		donnees_financieres.append(df1.loc[i])
-	else :
-		donnees_financieres.append(np.nan)
+df1 = df1.join(bdd, how = 'left')
+#print(df1)
+df1.to_csv('donnees_finales.csv', index = True, header = True)
+#donnees_financieres = []
 
-bdd['donnees_financieres'] = donnees_financieres
+#for i in companies_bdd :
+#	if i in companies_nace : 
+#		donnees_financieres.append(df1.loc[i])
+#	else :
+#		donnees_financieres.append(np.nan)
+
+#bdd['donnees_financieres'] = donnees_financieres
+
+
 
 
 #print(bdd.columns, '\n\n', bdd['donnees_financieres']['AT9150092530'].columns)
